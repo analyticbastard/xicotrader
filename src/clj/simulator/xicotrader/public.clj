187f492f63064d2b0740d420337502093b4c0d3e
Javier Arriero <javier.arriero@mirada.tv>
2017-07-19 07:59:30 +0200

    Simulator serves data using a lazy seq

--- src/clj/simulator/xicotrader/public.clj
+++ src/clj/simulator/xicotrader/public.clj
@@ -1,13 +1,23 @@
 (ns xicotrader.public
   (require
+    [clj-time.core :as time]
     [ring.util.http-response :as http-response]
     [cheshire.core :as cheshire]))
 
 (defn get-pairs []
   ["BTCEUR" "BTCETH" "ETHEUR"])
 
+(defn- generate-data [current-time]
+  (let [next-time (time/plus current-time (time/seconds 5))
+        last (+ 150. (* 10 (Math/random)))]
+    (lazy-seq (cons {:last last :time current-time} (generate-data next-time)))))
+
+(def data {"ETHEUR" (generate-data (time/now))})
+
 (defn get-tick [pair]
-  150.)
+  (->> (get data pair)
+       (filter #(time/after? (:time %) (time/now)))
+       (take 1)))
 
 (defn endpoint-pairs []
   (http-response/ok
