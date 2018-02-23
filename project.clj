1c92690f543ed1eb5b3692be86cc281c72ab1e77
javier <javierarrieropais@outlook.com>
2018-02-23 21:20:29 +0100

    Move web dependencies into simulator

--- project.clj
+++ project.clj
@@ -5,20 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.3.442"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [medley "1.0.0"]
                  [prismatic/schema "1.0.4"]
                  [com.stuartsierra/component "0.3.0"]
                  [cheshire "5.7.1"]
-                 [im.chit/cronj "1.4.4"]
                  [clj-http "3.6.1"]
-                 [com.cemerick/pomegranate "0.3.1"]
-
-                 ;; Web stuff
-                 [metosin/compojure-api "1.1.6" :exclusions [org.clojure/clojure]]
-                 [juxt.modular/ring "0.5.3" :exclusions [org.clojure/clojure]]
-                 [juxt.modular/http-kit "0.5.4" :exclusions [org.clojure/clojure]]
-                 [metosin/ring-http-response "0.6.5" :exclusions [ring/ring-core]]
-                 [ring/ring-json "0.4.0" :exclusions [org.clojure/clojure]]]
+                 [com.cemerick/pomegranate "0.3.1"]]
   :source-paths ["src/clj/common"]
   :test-paths ["test/clj/common"]
   :resource-paths ["resources/common"]
@@ -30,5 +23,11 @@
              :client {:source-paths ["src/clj/client" "src/clj/common"]
                       :resource-paths ["resources/client"]}
              :simulator {:source-paths ["src/clj/simulator" "src/clj/common"]
-                         :resource-paths ["resources/simulator" "resources/marketdata"]}
+                         :resource-paths ["resources/simulator" "resources/marketdata"]
+                         :dependencies [;; Web stuff
+                                        [metosin/compojure-api "1.1.6" :exclusions [org.clojure/clojure]]
+                                        [juxt.modular/ring "0.5.3" :exclusions [org.clojure/clojure]]
+                                        [juxt.modular/http-kit "0.5.4" :exclusions [org.clojure/clojure]]
+                                        [metosin/ring-http-response "0.6.5" :exclusions [ring/ring-core]]
+                                        [ring/ring-json "0.4.0" :exclusions [org.clojure/clojure]]]}
              :uberjar {:aot :all}})
